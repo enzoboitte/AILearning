f6b6999ce48d6f80838d6bef122d78542d10a4a0
enzoboitte <en.boitte@apiged.fr>
2026-03-03 16:13:32 +0100

    added documentation

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/enzo/Documents/c++/neuronal
-# Build directory: /home/enzo/Documents/c++/neuronal/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/build/Configure preset using toolchain file/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/enzo/Documents/c++/neuronal
-# Build directory: /home/enzo/Documents/c++/neuronal/out/build/Configure preset using toolchain file
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
